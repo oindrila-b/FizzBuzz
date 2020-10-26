01447e236abdeb83e3bcb6fc9272d05a8128214d
Oindrila Banerjee <ob.oindrila.banerjee@gmail.com>
2020-10-27 02:25:59 +0530

    Driver Class To Run FizzBuzz

---
+++ src/main/java/org/example/ob/fizzbuzz/Driver.java
@@ -0,0 +1,8 @@
+package org.example.ob.fizzbuzz;
+
+public class Driver {
+    public static void main(String args[]) {
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        fizzBuzz.printTillNum(100);
+    }
+}
