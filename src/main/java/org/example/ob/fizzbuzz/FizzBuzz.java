0ce13797b88d3107f1af78fa1ae66ac7318bbec7
Oindrila Banerjee <ob.oindrila.banerjee@gmail.com>
2020-10-27 01:57:02 +0530

    Fizz Equal Test

--- src/main/java/org/example/ob/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/example/ob/fizzbuzz/FizzBuzz.java
@@ -2,8 +2,20 @@ package org.example.ob.fizzbuzz;
 
 public class FizzBuzz {
 
-    public String getFizzBuzz(final int num) {
-        return null;
+    public boolean digitTest(int num, int expectedDigit) {
+        while (num > 0) {
+            if (num % 10 == expectedDigit) {
+                return true;
+            }
+            num = num / 10;
+        }
+        return false;
     }
 
+    public String getFizzBuzz(final int num) {
+        if ((num % 3 == 0) || (digitTest(num, 3))) {
+            return "Fizz";
+        }
+        return null;
+    }
 }
