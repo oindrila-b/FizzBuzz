d18ce066eb49a2504993ca02ceed36b1e01a7954
Oindrila Banerjee <ob.oindrila.banerjee@gmail.com>
2020-10-27 02:09:30 +0530

    FizzBuzz Equal Test

--- src/main/java/org/example/ob/fizzbuzz/FizzBuzz.java
+++ src/main/java/org/example/ob/fizzbuzz/FizzBuzz.java
@@ -13,11 +13,17 @@ public class FizzBuzz {
     }
 
     public String getFizzBuzz(final int num) {
-        if ((num % 3 == 0) || (digitTest(num, 3))) {
+        if ((num % 3 == 0) && (num % 5 == 0)) {
+            return "FizzBuzz";
+        }
+       else if ((num % 3 == 0) || (digitTest(num, 3))) {
             return "Fizz";
         }
         else if ((num % 5 == 0) || (digitTest(num, 5))) {
             return "Buzz";
+        }
+        else  if ((num % 3 == 0) && (num % 5 == 0)) {
+            return "FizzBuzz";
         }
 
         return null;
